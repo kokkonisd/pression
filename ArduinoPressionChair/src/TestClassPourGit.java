a8d4487af8194d41c635f37c774555720755d345
loche37 <lochejeremy@gmail.com>
2017-01-12 16:01:17 +0100

    Changing comment in testClass

--- ArduinoPressionChair/src/TestClassPourGit.java
+++ ArduinoPressionChair/src/TestClassPourGit.java
@@ -3,13 +3,8 @@ public class TestClassPourGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/* this method is pure awesome
-		 * trust me
-		 * 	it really
-		 * 		really
-		 * 			is
-		 */
+
+		//WESH J'ai tout efface :D
 
 	}
 
