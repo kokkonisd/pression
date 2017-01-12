8b78070b2f1a9aac850677d040b3a9eb56a5d145
Dimitris Kokkonis <kokkonisd@gmail.com>
2017-01-12 15:57:14 +0100

    test class changes

--- ArduinoPressionChair/src/TestClassPourGit.java
+++ ArduinoPressionChair/src/TestClassPourGit.java
@@ -3,6 +3,13 @@ public class TestClassPourGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/* this method is pure awesome
+		 * trust me
+		 * 	it really
+		 * 		really
+		 * 			is
+		 */
 
 	}
 
