466cbb216b89cee13573bb3ecfc9ff3ecaa28598
loche37 <lochejeremy@gmail.com>
2017-01-16 15:25:33 +0100

    Adding code to various classes

--- ArduinoPressionChair/src/Calculation/Chaise.java
+++ ArduinoPressionChair/src/Calculation/Chaise.java
@@ -11,10 +11,30 @@ public class Chaise {
 
 	public Chaise(){
 		Pieds=new ArrayList<>();
+		gposX=0;
+		gposY=0;
+	}
+
+	/**
+	 * Crée une chaise avec autant n=piedsNumber pieds.
+	 * Les id de pieds vont de 0 à piedsNumber-1 dans l'arrayList Pieds.
+	 * @param piedsNumber
+	 */
+	public Chaise(int piedsNumber){
+		Pieds=new ArrayList<>();
+		for(int i=0;i<piedsNumber;i++){
+			addPied(new Pied(i));
+		}
+		gposX=0;
+		gposY=0;
 	}
 
 	void addPied(Pied p){
 		Pieds.add(p);
+	}
+
+	Pied getPied(int piedIndex){
+		return Pieds.get(piedIndex);
 	}
 
 	void calculateGpos(){
@@ -65,6 +85,39 @@ public class Chaise {
 
 	public void setGposY(double gposY) {
 		this.gposY = gposY;
+	}
+
+	/**
+	 * Prends tout les Pieds de la liste Pieds de la chaise et attribut les valeurs
+	 * des capteurs aux pieds
+	 * @param piedsValues
+	 */
+	public void setPiedsValuesFromIntArray(int[] piedsValues){
+		for(int piedIndex=0;piedIndex<Pieds.size();piedIndex++){
+			int value=piedsValues[piedIndex];
+			Pieds.get(piedIndex).setValue(value);
+		}
+	}
+
+	public double getMaxPosX(){
+		double max=0;
+		for(int i=0;i<Pieds.size();i++){
+			double posX=Pieds.get(i).getPosX();
+			if(posX>max){
+				max=posX;
+			}
+		}
+		return max;
+	}
+	public double getMaxPosY(){
+		double max=0;
+		for(int i=0;i<Pieds.size();i++){
+			double posY=Pieds.get(i).getPosY();
+			if(posY>max){
+				max=posY;
+			}
+		}
+		return max;
 	}
 
 
--- ArduinoPressionChair/src/gui/PostureVisulatizationJPanel.java
+++ ArduinoPressionChair/src/gui/PostureVisulatizationJPanel.java
@@ -1,14 +1,26 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.LayoutManager;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+import Calculation.Chaise;
+import Calculation.Pied;
+
 public class PostureVisulatizationJPanel extends JPanel {
 
+	private Chaise chaise;
+
+	public static final int circleDiameter=5;
+
 	public PostureVisulatizationJPanel() {
-		// TODO Auto-generated constructor stub
+		this.chaise=new Chaise();
 	}
+
+	/*
 
 	public PostureVisulatizationJPanel(LayoutManager arg0) {
 		super(arg0);
@@ -24,5 +36,43 @@ public class PostureVisulatizationJPanel extends JPanel {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
+	*/
+
+	public void setChaise(Chaise chaise){
+		this.chaise=chaise;
+	}
+
+	public Chaise getChaise(){
+		return this.chaise;
+	}
+
+	public void paintComponent(Graphics g){
+		int width=getWidth();
+		int height=getHeight();
+
+		g.setColor(Color.BLACK);
+
+		ArrayList<Pied> Pieds=chaise.getPieds();
+		for(int i=0;i<Pieds.size();i++){
+			Pied pied=Pieds.get(i);
+			g.drawOval(scaleX(pied.getPosX()), scaleY(pied.getPosY()), circleDiameter, circleDiameter);
+		}
+
+	}
+
+	public int scaleX(double posX){
+		int width=getWidth();
+		int Xvalue=(int)(width*(posX/(chaise.getMaxPosX())));
+		return Xvalue;
+	}
+
+	public int scaleY(double posY){
+		int width=getHeight();
+		int Yvalue=(int)(width*(posY/(chaise.getMaxPosX())));
+		return Yvalue;
+	}
+
+
+
 
 }
