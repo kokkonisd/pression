e91557106f84abd56ceb7a9d249d8686c9fcff12
loche37 <lochejeremy@gmail.com>
2017-04-25 11:39:17 +0200

    Corrected the last part of the report
    Also started the conclusion and tried to add a bibliography (no succes)

--- ArduinoPressionChair/src/gui/TestMainWindow.java
+++ ArduinoPressionChair/src/gui/TestMainWindow.java
@@ -4,53 +4,78 @@ import java.awt.HeadlessException;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.SwingUtilities;
+
 import Calculation.Chaise;
 import Calculation.Pied;
 
 public class TestMainWindow {
 
 	public static void main(String[] args) throws InterruptedException, HeadlessException, ClassNotFoundException, IOException {
-		MainWindow win=new MainWindow();
 
-		Chaise chaise=new Chaise();
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run() {
+				MainWindow win;
+				try {
+					win = new MainWindow();
+				} catch (HeadlessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				}
 
-		Pied p1=new Pied(0.0, 0.0, 1);
-		Pied p2=new Pied(1.0, 0.0, 2);
-		Pied p3=new Pied(0.0, 1.0, 3);
-		Pied p4=new Pied(1.0, 1.0, 4);
+				Chaise chaise=new Chaise();
 
-		p1.setValue(5);
-		p2.setValue(5);
-		p3.setValue(5);
-		p4.setValue(5);
+				Pied p1=new Pied(0.0, 0.0, 1);
+				Pied p2=new Pied(1.0, 0.0, 2);
+				Pied p3=new Pied(0.0, 1.0, 3);
+				Pied p4=new Pied(1.0, 1.0, 4);
 
-		chaise.addPied(p1);
-		chaise.addPied(p2);
-		chaise.addPied(p3);
-		chaise.addPied(p4);
+				p1.setValue(5);
+				p2.setValue(5);
+				p3.setValue(5);
+				p4.setValue(5);
 
-		chaise.calculateGpos();
-		System.out.println(chaise.getGposX());
-		System.out.println(chaise.getGposY());
+				chaise.addPied(p1);
+				chaise.addPied(p2);
+				chaise.addPied(p3);
+				chaise.addPied(p4);
 
-		System.out.println(chaise.getMaxPosX());
-		System.out.println(chaise.getMaxPosY());
+				chaise.calculateGpos();
+				System.out.println(chaise.getGposX());
+				System.out.println(chaise.getGposY());
 
-		// initialize deadzone
-		chaise.setDeadzoneRatio(0.5);
+				System.out.println(chaise.getMaxPosX());
+				System.out.println(chaise.getMaxPosY());
 
-		chaise.setDeadzoneX(chaise.getGposX());
-		chaise.setDeadzoneY(chaise.getGposY());
+				// initialize deadzone
+				chaise.setDeadzoneRatio(0.5);
 
-		/*
+				chaise.setDeadzoneX(chaise.getGposX());
+				chaise.setDeadzoneY(chaise.getGposY());
+
+				/*
 		System.out.println(chaise.isGinDeadzone());
 
 
 		chaise.setGposX(1);
 		chaise.setGposY(1);
 		System.out.println(chaise.isGinDeadzone());
-		*/
+				 */
 
-		win.setChaise(chaise);
+				win.setChaise(chaise);
+			}
+		});
+
+
 	}
 }
